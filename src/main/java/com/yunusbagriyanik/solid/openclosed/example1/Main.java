b2c8b9fe5cb6b958266bae9d71c3b7187bb8f70a
Yunus Bagriyanik <yunusbgrynk@gmail.com>
2023-10-27 21:05:48 +0300

    area of ​​triangle

--- src/main/java/com/yunusbagriyanik/solid/openclosed/example1/Main.java
+++ src/main/java/com/yunusbagriyanik/solid/openclosed/example1/Main.java
@@ -3,6 +3,7 @@ package com.yunusbagriyanik.solid.openclosed.example1;
 import com.yunusbagriyanik.solid.openclosed.example1.noncompliant.ShapeNonCompliant;
 import com.yunusbagriyanik.solid.openclosed.example1.noncompliant.model.CircleV1;
 import com.yunusbagriyanik.solid.openclosed.example1.noncompliant.model.RectangleV1;
+import com.yunusbagriyanik.solid.openclosed.example1.noncompliant.model.TriangleV1;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
@@ -15,5 +16,8 @@ public class Main {
         double v2 = shapeNonCompliant.calculateArea(List.of(new RectangleV1(5, 10), new RectangleV1(5, 5)));
         log.info("V1: {}", v1);
         log.info("V2: {}", v2);
+
+        double v3 = shapeNonCompliant.calculateArea(List.of(new TriangleV1(5, 10)));
+        log.info("V3: {}", v3);
     }
 }
